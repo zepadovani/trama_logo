c30462dc2c30a74c5251379b35146f8685626561
josé henrique padovani <zepadovani@gmail.com>
2022-10-03 10:12:35 -0300

    trama logo - v0.1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# trama_logo
+# A webpack template to work with p5js
+
+...still working on this ;)
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './style/main.css'
+import p5 from 'p5';
+import p5Svg from "p5.js-svg"
+
+p5Svg(p5);
+
+
+import sketch from './sketch';
+
+// let p5sk;
+// let p5sk;
+// window.onload = () => {
+//   p5sk = new p5(sketch);
+// };
+
+
+// p5sk = new p5(sketch, 'p5container');
+new p5(sketch, 'p5container');
+
+
+
+// window.onload = () => {
+//     console.log("newload")
+//     ps5k = new p5(sketch, 'p5container');
+// };
+
